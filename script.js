cbb693886864f095d8543c258ac73990dbe21af3
Roman <githubkrvchk@gmail.com>
2019-05-13 15:45:44 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-window.onload = function() {
+window.onload = function () {
     let links = document.getElementById('links').children;
     let logoMain = document.getElementById('logoMain');
 
-    let scrollMain = document.getElementById('scrollMain');
-    let scrollAbout = document.getElementById('scrollAbout');
+
+    let scrollSection1 = document.getElementById('scrollSection1');
+    let scrollSection2 = document.getElementById('scrollSection2');
+    let scrollSection3 = document.getElementById('scrollSection3');
+    let scrollSection4 = document.getElementById('scrollSection4');
 
     const scrollToElementsLinks = [
-        { link: links[0], element: scrollMain},
-        { link: links[1], element: scrollAbout},
-        { link: links[2], element: scrollAbout}
+        { link: logoMain, element: scrollSection1 },
+        { link: links[0], element: scrollSection2 },
+        { link: links[1], element: scrollSection3 },
+        { link: links[2], element: scrollSection4 }
     ];
 
-    logoMain.addEventListener('click' , () => scrolling(scrollMain));
     for (let linkEvent of scrollToElementsLinks) {
         const { link, element } = linkEvent;
 
-        link.addEventListener('click' , () => scrolling(element));
+        link.addEventListener('click', () => scrolling(element));
     }
 
     function scrolling(element) {
         event.preventDefault();
-        element.scrollIntoView({block: "start", behavior: "smooth"});
+
+        if (element.getAttribute('down')) {
+            element.scrollIntoView({ block: "end", behavior: "smooth" });
+            return;
+        }
+
+        element.scrollIntoView({ block: "start", behavior: "smooth" });
     }
 }
